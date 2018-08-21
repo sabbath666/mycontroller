d0b6bdd359f1f82afe27cda00596053a8c4f07e2
sabbath <sergeysabbath@gmail.com>
2018-08-21 13:28:30 +0300

    first commit

---
+++ src/main/kotlin/com/sabbath/mycontroller/MycontrollerApplication.kt
@@ -0,0 +1,17 @@
+package com.sabbath.mycontroller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class MycontrollerApplication{
+    @GetMapping("/test")
+    fun test()="ok"
+}
+
+fun main(args: Array<String>) {
+    runApplication<MycontrollerApplication>(*args)
+}
