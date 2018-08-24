c098400234c9bfdc19f0aaff0314c66a4c6ac4dd
sabbath <sergeysabbath@gmail.com>
2018-08-24 20:13:38 +0300

    added jenkinsfile

--- src/main/kotlin/com/sabbath/mycontroller/MycontrollerApplication.kt
+++ src/main/kotlin/com/sabbath/mycontroller/MycontrollerApplication.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MycontrollerApplication{
     @GetMapping("/test")
-    fun test()="hello AAAAAAAA"
+    fun test()="hello BBBBBB"
 }
 
 fun main(args: Array<String>) {
